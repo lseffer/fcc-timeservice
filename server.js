5feecc94a0a35b687acd2c306b7887cbb48f37b4
Leonard Seffer <leonardseffer@gmail.com>
2017-03-15 19:22:18 +0000

    time server accepting unix and human readable time formats as parameters to the base url

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ var express = require("express");
 
 var app = express();
 
-app.get('/', function(req, res){
-    res.end('hello world');
+app.get('/:date', function(req, res){
+    // res.send('try a string after the url');
+    var inputurl = req.params.date;
+    var outputdate;
+    var output = {"date" : null, "unix": null};
+    if(typeof parseInt(inputurl, 10) === 'number' & !isNaN(parseInt(inputurl, 10))){
+        outputdate = new Date(parseInt(inputurl, 10)*1000);
+        output["date"] = outputdate.toDateString();
+        output["unix"] = outputdate.getTime() / 1000;
+    } else {
+        outputdate = new Date(Date.parse(inputurl));
+        if(!isNaN(outputdate)){
+        output["date"] = outputdate.toDateString();
+        output["unix"] = outputdate.getTime() / 1000;
+        }
+    }
+    
+    res.end(JSON.stringify(output, null, 4));
 });
 
 app.listen(8080, function(){
